421ec739b6bd611e1f658c2935be1e6781866ba1
kcancurly <karadumankcan@gmail.com>
2025-08-01 19:29:51 +0300

    u

--- WebArchive.py
+++ WebArchive.py
@@ -318,11 +318,8 @@ def save_raw_data(domain: str, urls: List[str], output_dir: str) -> str:
 def dns_check_domain(subdomains):
     valid_subdomains = []
     resolver = dns.resolver.Resolver()
-    resolver.lifetime = 10
-    resolver.nameservers = ["8.8.8.8"]
     for domain in subdomains:
         try:
-
             answers = resolver.resolve(domain, "A")
             if answers:
                 valid_subdomains.append(domain)
